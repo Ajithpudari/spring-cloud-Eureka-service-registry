c027434a5d52dd5c24c9cf47ab9da48d328a504b
ajithpudari <ajithpudari@gmail.com>
2022-04-22 00:26:47 +0530

    spring cloud Eureka

---
+++ amazon-server/src/test/java/com/ajtechie/spring/eureaka/server/AmazonServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ajtechie.spring.eureaka.server;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmazonServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
